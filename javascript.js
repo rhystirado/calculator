d78baecbf693b17bcbfdc89c30f5bfd37e450af5
Rhys Tirado <rhys.tirado@gmail.com>
2022-05-12 15:35:10 +1000

    Add display operators and compute function

--- javascript.js
+++ javascript.js
@@ -41,8 +41,6 @@ const calculatorContainer = document.querySelector('.calculator-container');
 // Add display to the calculator
 const display = document.createElement('div');
 display.classList.add('calculator-display');
-const displayText = '';
-display.textContent = displayText;
 calculatorContainer.append(display);
 
 // Add buttons to the calculator
@@ -59,21 +57,74 @@ for (const button of buttonList) {
   calculatorContainer.append(buttonElement);
 }
 
-// 
 
-// Populate the display with the digits when pressed
+// Populate the display with the digits and operators when pressed
 const buttons = Array.from(document.querySelectorAll('button'));
 const digitButtons = buttons.filter(button => {
   return !isNaN(button.textContent) && button.textContent;
 });
+
 
 // Add event listener to each button to add digit to display
 digitButtons.forEach(button => {
   button.addEventListener('click', displayDigit);
 })
 
-// Write digit to display
+// Operator buttons
+const operatorButtons = buttons.filter(button => {
+  const operators = ['+', '-', '*', '/'];
+  return operators.includes(button.textContent);
+});
+
+// Write digit to display and enable operator buttons
 function displayDigit() {
   const digit = this.textContent;
   display.textContent += digit;
+
+  operatorButtons.forEach(button => {
+    button.addEventListener('click', displayOperator);
+  })
 }
+
+// Write operator to display and disable button
+// Enabled once a digit, or equals has been pressed
+function displayOperator() {
+  const operator = this.textContent;
+  display.textContent += operator;
+  operatorButtons.forEach(button => {
+    button.removeEventListener('click', displayOperator);
+  });
+}
+
+// Clear display when clear button is pressed
+const clearButton = buttons.find(button => button.textContent === 'AC');
+clearButton.addEventListener('click', clearDisplay);
+
+function clearDisplay() {
+  display.textContent = '';
+}
+
+// Take a string with one operator and convert it to the numbers and the operator
+function splitInput(str) {
+  // Split on the operator and group the delimeter to keep it
+  const parts = str.split(/([\+\-\*\/])/g);
+  const input = {
+    a: parts[0],
+    b: parts[2],
+    operator: parts[1]
+  }
+  return input;
+}
+
+// Get the display string and compute the answer
+function compute(str) {
+  // Get input from calculator display
+  const input = splitInput(str);
+  return operate(input.operator, input.a, input.b);
+}
+
+
+// console.log(compute('1+2'));
+// console.log(compute('1-2'));
+// console.log(compute('1*2'));
+// console.log(compute('1/2'));
