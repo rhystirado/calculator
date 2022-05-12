a119675cf6b8adca6f7c947df26453f74330ff20
Rhys Tirado <rhys.tirado@gmail.com>
2022-05-12 13:44:04 +1000

    Add multiply, subtract, divide

--- javascript.js
+++ javascript.js
@@ -3,4 +3,24 @@ function add(arr) {
   return arr.reduce((sum, current) => sum + current);
 }
 
+// Add two numbers together
+function addTwo(a, b) {
+  return a + b;
+}
+
+// Subtract two numbers
+function subtract(a, b) {
+  return a - b;
+}
+
+// Multiply two numbers
+function multiply(a, b) {
+  return a * b;
+}
+
+// Divide two numbers
+function divide(a, b) {
+  return a / b;
+}
+
 console.log(add([1, 2, 3, 4]));
